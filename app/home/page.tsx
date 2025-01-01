14dca042dba8cb6c6000f98c214cf2aeb294036d
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-01 23:54:54 +0100

    add heading and update Home component styling with gradient background

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,8 +4,9 @@ const Home = () => {
   return (
     <>
       <div>
-        <div>
-          crad
+        <h1 className='text-h1'>Home</h1>
+        <div className='bg-gradient-to-r from-[#4468A1] to-[#706a6a] gap-4 flex flex-col px-4 pt-8 pb-12 md:w-full lg:w-full md:items-center'>
+          home
         </div>
         <div>
           home
