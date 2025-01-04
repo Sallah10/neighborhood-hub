f6db9ad75495ba9bdc70daf4ff0a8796eeafe040
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-04 20:38:27 +0100

    success page done

--- app/subscribe.tsx
+++ app/subscribe.tsx
@@ -20,16 +20,16 @@ const Subscribe = () => {
                     <button className='px-4 py-2 text-[#E4E4E4] border-2 border-[#878787]'>$200</button>
                     <button className='px-4 py-2 text-[#E4E4E4] border-2 border-[#878787]'>$300</button>
                 </div>
-                <div className='flex gap-4 self-center w-full'>
+                <div className='flex justify-between px-6 w-full'>
                     <h3 className='text-[#878787] text-p'>Tax</h3>
                     <h3 className='text-[#364C6F] text-p'>$20</h3>
                 </div>
-                <div className='flex justify-between'>
+                <div className='flex justify-between px-6 w-full'>
                     <h3 className='text-[#878787] text-p'>Commence Fees</h3>
                     <h3 className='text-[#364C6F] text-p'>$5</h3>
                 </div>
-                <div className='text-[#364C6F] text-h3 flex justify-between'>
-                    <h3>Total Amount to be paid</h3>
+                <div className='text-[#364C6F] text-p flex justify-between px-2 w-full border-t-2 pt-4 border-[#878787] mx-2'>
+                    <h3>Total Amount to be paid:</h3>
                     <h3>$125</h3>
                 </div>
             </div>
--- app/success.tsx
+++ app/success.tsx
@@ -1,8 +1,21 @@
+'use client';
 import React from 'react'
+import Image from 'next/image';
+import star from './assets/success.png'
+import tick from './assets/check.png'
 
 const Success = () => {
     return (
-        <div>Success</div>
+        <>
+            <div className='rounded-2xl flex flex-col bg-[#EEEEEE] items-center py-6 px-2 w-full gap-4'>
+                <Image src={star} alt='sucess' className='' />
+                <Image src={tick} alt='sucess' className='-mt-16 mb-6' />
+                <h2 className='flex self-center text-h2'>Hurray You Transfered</h2>
+                <h2 className='text-h2'>$125</h2>
+                <h2 className='text-h2'>To</h2>
+                <p className='rounded-md px-6 py-4 text-p text-[#3C8476] text-center'>Ev Startup Business Account: 3244561783</p>
+            </div>
+        </>
     )
 }
 
