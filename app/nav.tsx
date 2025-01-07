7fc281eb64bead96441c80cab727683dbabbe774
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-07 16:09:06 +0100

    add logo and avatar images to navigation component

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import Image from 'next/image'
+import avatar from '../assets/avatar.png'
 
 const Nav = () => {
   return (
-    <div className='text-black bg-clip-content'>Nav</div>
+    <>
+      <div className='text-black hidden'>
+        <Image src={logo} alt='logo' />
+        <div>
+          <a href="">Dashboard</a>
+          <a href="">View campaign</a>
+          <a href="">Fundraiser</a>
+        </div>
+        <Image src={avatar} alt="avatar" />
+      </div>
+    </>
   )
 }
 
