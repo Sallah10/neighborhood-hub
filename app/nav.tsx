9257130452e76c21d225a0c92f0468220bf679d0
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-07 19:54:25 +0100

    navBar done and dusted

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import logo from '../assets/logo.png'
+import logo from './assets/logo.png'
 import Image from 'next/image'
-import avatar from '../assets/avatar.png'
+import avatar from './assets/avatar.png'
 
 const Nav = () => {
   return (
     <>
-      <div className='text-black hidden'>
-        <Image src={logo} alt='logo' />
-        <div>
+      <div className='text-black hidden md:flex justify-between items-center p-4'>
+        <Image src={logo} alt='logo' className='w-[150px] h-[70px]' />
+        <div className='text-h3 flex gap-6'>
           <a href="">Dashboard</a>
           <a href="">View campaign</a>
           <a href="">Fundraiser</a>
